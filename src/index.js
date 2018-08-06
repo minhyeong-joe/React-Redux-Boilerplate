877c2dc35354f20cf403f78a1f71fd0f8a312072
minhyeong-joe <minhyeong.billy.joe@gmail.com>
2018-08-05 22:12:44 -0700

    replace react-router with react-router-dom + router basic setup

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import ReduxPromise from 'redux-promise';
@@ -13,7 +14,11 @@ const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
+    <BrowserRouter>
+      <div>
+        <Route exact path="/" component={App} />
+      </div>
+    </BrowserRouter>
   </Provider>
   , document.getElementById('root')
 );
